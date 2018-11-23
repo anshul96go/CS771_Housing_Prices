61d22f94accb80d1f913e436795c216167f13ebc
navanya <30443234+navanya@users.noreply.github.com>
2018-11-24 00:32:50 +0530

    Code and Graph of MSE/Lambda for ridge Regression
    I have divided the data as decided in the group. The MSE for the prediction is lowest for lambda equal to zero that is linear regression itself

---
+++ ProjectCS771.m
@@ -0,0 +1,19 @@
+k=xlsread('train',1,'A1:BT1452');
+y=k(1:1162,20);
+yt=k(1162:1451,20);
+x=[ones(1162,1),k(1:1162,1:19),k(1:1162,21:72)];
+xt=[ones(290,1),k(1162:1451,1:19),k(1162:1451,21:72)];
+
+l=[0:0.1:5,5:1:100];
+mse=zeros(size(l,2),1);
+for i=1:1:size(l,2)
+b=(x'*x+l(i)*eye(72))^-1*x'*y;
+mse(i)=sqrt(sum((xt*b-yt).^2)/290);
+end
+yb=mean(y);
+ssr=sum((x*b-yb*(ones(1162,1))).^2);
+sst=sum((y-yb*(ones(1162,1))).^2);
+r=ssr/sst;
+plot(l,mse)
+xlabel('lambda')
+ylabel('standard deviation')
