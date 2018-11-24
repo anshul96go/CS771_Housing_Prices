e18d22d30bab08acdb6155ac5e8594cdd62c36a2
Pawan Harendra Mishra <pawanmsr@outlook.com>
2018-11-24 23:50:47 +0530

    output results

--- lasso.py
+++ lasso.py
@@ -26,12 +26,13 @@ def lasso_regression(X_train, X_validate, y_train, y_validate, alpha):
     reg.fit(X_train,y_train)
     y_pred = reg.predict(X_validate)
     mse = mean_squared_error(y_validate,y_pred)
-    print("RMSE:", math.sqrt(mse))
-    return reg.coef_, y_pred
+    rmse = math.sqrt(mse)
+    print("RMSE:", rmse)
+    return reg.coef_, y_pred, rmse
 
 def custom_plot(column, y_pred, y_validate, x):
     plt.figure(figsize=[15,10])
-    plt.scatter(x, y_pred, color='r', label="Regression Line")
+    plt.scatter(x, y_pred, color='r', label="Predicted Points")
     plt.scatter(x, y_validate, color='g', label="Sales Price")
     plt.legend()
     plt.title(saveName+' '+column)
@@ -44,11 +45,23 @@ columns = data.drop('SalePrice', axis=1).columns.values
 y = data['SalePrice'].values
 X_train, X_validate, y_train, y_validate = train_test_split(X,y,test_size=0.2)
 alpha = cv_estimator(X_train,y_train)
-w, y_pred = lasso_regression(X_train, X_validate, y_train, y_validate, alpha)
+w, y_pred, rmse = lasso_regression(X_train, X_validate, y_train, y_validate, alpha)
 print(columns.shape, w.shape, X_validate.shape)
+selected_columns = []
+
 count = 0
 for i in range(len(w)):
     if abs(int(w[i])) > 0:
-        custom_plot(columns[i], y_pred, y_validate, X_validate[:,i])
+        #custom_plot(columns[i], y_pred, y_validate, X_validate[:,i])
+        selected_columns.append(columns[i])
         count+=1
 print(count)
+
+with open(saveName+'_'+'results.txt', 'w') as f:
+    f.write('Number of features: ' + str(w.shape[0]) + '\n')
+    f.write('Features with non zero weight: ' + str(count) + '\n')
+    f.write('Best regularization parameter: ' + str(alpha) + '\n')
+    f.write('Root Mean Squared Error: ' + str(rmse) + '\n')
+    f.write('Selected Columns:\n')
+    for column in selected_columns:
+        f.write(column + '\n')
