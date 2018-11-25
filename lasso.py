36bc7fe93c40532adc8f125ac2c0aae1da31563f
Pawan Harendra Mishra <pawanmsr@outlook.com>
2018-11-25 11:36:01 +0530

    add plots

--- lasso.py
+++ lasso.py
@@ -10,6 +10,7 @@ import os
 
 saveName = "lasso"
 data_file = "data/clean_train.csv"
+selected_features_file = "data/selected_features.csv"
 alphas = [0.01, 0.1, 1, 10, 100, 1000, 10000]
 
 imagedir = 'images/'
@@ -30,13 +31,15 @@ def lasso_regression(X_train, X_validate, y_train, y_validate, alpha):
     print("RMSE:", rmse)
     return reg.coef_, y_pred, rmse
 
-def custom_plot(column, y_pred, y_validate, x):
+def custom_plot(name, y_pred, y_validate, x):
     plt.figure(figsize=[15,10])
     plt.scatter(x, y_pred, color='r', label="Predicted Points")
     plt.scatter(x, y_validate, color='g', label="Sales Price")
     plt.legend()
-    plt.title(saveName+' '+column)
-    plt.savefig(imagedir+saveName+'_'+column+'.png')
+    plt.xticks(x, [])
+    plt.grid(True, axis='both', linewidth=0.2)
+    plt.title(saveName+' '+name)
+    plt.savefig(imagedir+saveName+'_'+name+'.png')
     plt.close()
 
 data = pd.read_csv(data_file, index_col='Id')
@@ -46,22 +49,31 @@ y = data['SalePrice'].values
 X_train, X_validate, y_train, y_validate = train_test_split(X,y,test_size=0.2)
 alpha = cv_estimator(X_train,y_train)
 w, y_pred, rmse = lasso_regression(X_train, X_validate, y_train, y_validate, alpha)
-print(columns.shape, w.shape, X_validate.shape)
+# print(columns.shape, w.shape, X_validate.shape)
 selected_columns = []
+selected_features_columns = pd.read_csv(selected_features_file).columns.values
+intersection_columns = []
 
-count = 0
 for i in range(len(w)):
     if abs(int(w[i])) > 0:
-        #custom_plot(columns[i], y_pred, y_validate, X_validate[:,i])
         selected_columns.append(columns[i])
-        count+=1
-print(count)
+        if columns[i] in selected_features_columns:
+            intersection_columns.append(columns[i])
+print(len(selected_columns))
+print(len(intersection_columns))
+
+custom_plot('Prediction', y_pred, y_validate, range(1,len(y_pred)+1))
+custom_plot('Prediction_Magnified', y_pred[20:50], y_validate[20:50], range(1,len(y_pred[20:50])+1))
 
 with open(saveName+'_'+'results.txt', 'w') as f:
     f.write('Number of features: ' + str(w.shape[0]) + '\n')
-    f.write('Features with non zero weight: ' + str(count) + '\n')
+    f.write('Number of features with non zero weight: ' + str(len(selected_columns)) + '\n')
+    f.write('Number of features with non zero weight in selected features: ' + str(len(intersection_columns)) + '\n')
     f.write('Best regularization parameter: ' + str(alpha) + '\n')
-    f.write('Root Mean Squared Error: ' + str(rmse) + '\n')
-    f.write('Selected Columns:\n')
+    f.write('Root Mean Squared Error: ' + str(rmse) + '\n\n')
+    f.write('Features with non zero weight:\n')
     for column in selected_columns:
+        f.write(column + '\n')
+    f.write('\nFeatures with non zero weights in selected features:\n')
+    for column in intersection_columns:
         f.write(column + '\n')
